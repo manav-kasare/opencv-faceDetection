0d9f905600a6275c01b0976e8971ee67f1dcd6c7
manav-kasare <kmanav0705@gmail.com>
2021-09-17 22:10:36 +0530

    img -> frame

--- video.py
+++ video.py
@@ -5,16 +5,16 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture('test-video.mp4')
 
 while True:
-    _, img = cap.read()
+    _, frame = cap.read()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    cv2.imshow('img', img)
+    cv2.imshow('frame', frame)
 
     k = cv2.waitKey(30) & 0xff
     if k==27:
